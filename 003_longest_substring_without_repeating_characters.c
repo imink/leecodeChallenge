98a732044c84db1bd395ad67e871f1288bb8acd7
Yue Wang <imink@iminks-MacBook-Pro.local>
2015-07-23 12:04:49 +0800

    normal solution 003

--- 003_longest_substring_without_repeating_characters.c
+++ 003_longest_substring_without_repeating_characters.c
@@ -9,11 +9,41 @@
 
 /**
  * using hashing map to check if the next one is already in the list
- * 
- * @param  s [description]
+ * once find a repeated char, compute the current length and compare it 
+ * to the max previous length.
+ * @param   [description]
  * @return   [description]
  */
 int lengthOfLongestSubstring(char *s)
 {
+
+	int length = s.length;
+	int curLength = 0, maxLength = 0;
+	char archive[length] = {false};
+
+	int i = 0, j = 0;
+
+	while(s[j++]){
+
+		i = j + 1;
+		while(s[i++]){
+
+			if(!archive[s[i]]){
+				// not repeated, marked as true
+				archive[s[i]] = true; 
+				curLength ++; // current length increment 
+			} else {
+				// found repeated, break traverse,compare with previous max length
+				break; // jump out of current loop
+			}
+		}
+
+		if(curLength > maxLength ) {
+			maxLength = curLength;
+		}
+
+	}
+
+	return maxLength;
 
 }
--- README.md
+++ README.md
@@ -4,8 +4,10 @@ A repository records everyday's leetcode practice
 
 
 Reference:
-[Tgic leetcode solution](http://leetcode.tgic.me)
+[Tgic Leetcode Solution](http://leetcode.tgic.me)
 
 [ACM之家](http://www.acmerblog.com/data-structure-algorithm-6107.html)
 
-[Coolshell leetcode practice](http://coolshell.cn/articles/12052.html)
+[Coolshell Leetcode Practice](http://coolshell.cn/articles/12052.html)
+
+[LeetCode Online Port for IT Interview](http://articles.leetcode.com/2011/05/longest-substring-without-repeating-characters.html)
