efe2d0d7198bc051537e7e3e60829647b9758300
Patrick <patrick.wang@gmail.com>
2016-10-23 16:04:14 +0100

    add lintcode

---
+++ lintcode/src/LintCode0003.java
@@ -0,0 +1,59 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by imink on 23/10/2016.
+ */
+public class LintCode0003 {
+    private PriorityQueue<Integer> maxHeap, minHeap;
+    private int index = 0;
+
+    public int[] medianII(int[] nums) {
+        Comparator<Integer> descenCmp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        int inputLength = nums.length;
+        maxHeap = new PriorityQueue<Integer>(inputLength, descenCmp);
+        minHeap = new PriorityQueue<Integer>(inputLength);
+
+        int[] results = new int[inputLength];
+        for (int i = 0; i < inputLength; i ++) {
+            addNumber(nums[i]);
+            results[i] = getMedian();
+        }
+
+        return results;
+    }
+
+    void addNumber(int number) {
+        maxHeap.add(number);
+        // if is even
+        if (index % 2 == 0) {
+            if (minHeap.isEmpty()) {
+                index ++;
+                return;
+            } else if (maxHeap.peek() > minHeap.peek()) {
+                // TODO: 23/10/2016 why Integer not int
+                Integer maxHeapRoot = maxHeap.poll();
+                Integer minHeapRoot = minHeap.poll();
+                minHeap.add(maxHeapRoot);
+                maxHeap.add(minHeapRoot);
+            }
+        }
+        // if is odd
+        else {
+            minHeap.add(maxHeap.poll());
+        }
+        index ++;
+    }
+
+
+    int getMedian() {
+        return maxHeap.peek();
+    }
+
+}
